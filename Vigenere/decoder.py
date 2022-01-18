39c73173f65f5b087a03f391c31d226b43a3164d
oOperaho <emanuelllevi7770@gmail.com>
2022-01-18 20:15:14 -0300

    Init

---
+++ Vigenere/decoder.py
@@ -0,0 +1,37 @@
+from operator import add
+
+word = str(input("Type the word to be encrypted: ")).strip().lower().replace(" ", "")
+key = str(input("Type the vigenere key: ")).strip().lower().replace(" ", "")
+nkey = 0
+res, w, letters = "", "", " abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+
+if len(word) > len(key):
+    nkey = len(word) - len(key)
+    w = key[:nkey]
+    res = key + w
+
+elif len(word) < len(key):
+    nkey = len(key) - len(word)
+    w = key[:-nkey]
+    res += w
+
+else:
+    res += key
+
+s, l, fin, enc = [], [], [], []
+
+for i in word:
+    s.append(letters.find(i))
+
+for n in range(0, len(s)):
+    s[n] = s[n] - 1
+
+for j in res:
+    l.append(letters.find(j))
+
+fin = list(map(add, s, l))
+
+for x in fin:
+    enc.append(letters[x])
+
+print("".join(enc))
