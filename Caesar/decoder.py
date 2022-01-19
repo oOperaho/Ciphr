11dcd05e1a5d9879a08fe8cdde622cf1275f491b
oOperaho <emanuelllevi7770@gmail.com>
2022-01-18 21:51:37 -0300

    → some fixes

--- Caesar/decoder.py
+++ Caesar/decoder.py
@@ -1,26 +1,9 @@
 word = str(input("Enter the word to encrypt: ")).strip().lower().replace(" ", "")
 key = int(input("Enter the key (0-10): "))
-alphabet = " abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+alphabet, out, l = " abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "", []
 
+for letter in word:
+    out = alphabet.index(letter) + key
+    l.append(alphabet[out])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("".join(l))
