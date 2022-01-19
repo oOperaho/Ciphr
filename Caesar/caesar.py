caf9f32a6dbcc65aebd351346a866a2b50aeba85
oOperaho <emanuelllevi7770@gmail.com>
2022-01-18 21:57:00 -0300

    → Done.

--- Caesar/caesar.py
+++ Caesar/caesar.py
@@ -1,5 +1,5 @@
 word = str(input("Enter the word to encrypt: ")).strip().lower().replace(" ", "")
-key = int(input("Enter the key (0-26): "))
+key = int(input("Enter the caesar key (0-26): "))
 alphabet, out, l = " abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "", []
 
 for letter in word:
