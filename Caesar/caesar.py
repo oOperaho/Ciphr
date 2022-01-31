5443ffa477516cdd84b8ba50cd8d02c162467891
zF4ke <pedrohsilva955@gmail.com>
2022-01-31 21:01:12 +0000

    reformat thing

--- Caesar/caesar.py
+++ Caesar/caesar.py
@@ -1,24 +1,26 @@
+import re
+
+
+def reformat(txt):
+    ntxt = re.sub("[^a-zA-Z]", "", txt)
+    return ntxt
+
+
 def caesarencoder(w, k):
-    try:
-        alphabet, out, l = " abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "", []
+    alphabet, out, l = " abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "", []
 
-        for letter in w.lower():
-            out = alphabet.index(letter) + k
-            l.append(alphabet[out])
+    for letter in reformat(w):
+        out = alphabet.index(letter) + k
+        l.append(alphabet[out])
 
-        return "".join(l)
-    except TypeError:
-        return "Error! Type a number as key!"
+    return "".join(l)
 
 
 def caesardecoder(w, k):
-    try:
-        alphabet, out, l = " zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcba", "", []
+    alphabet, out, l = " zyxwvutsrqponmlkjihgfedcbazyxwvutsrqponmlkjihgfedcba", "", []
 
-        for letter in w.lower():
-            out = alphabet.index(letter) + k
-            l.append(alphabet[out])
+    for letter in reformat(w):
+        out = alphabet.index(letter) + k
+        l.append(alphabet[out])
 
-        return "".join(l)
-    except TypeError:
-        return "Error! Type a number as key!"
+    return "".join(l)
