b774c9a216ea6bfbce8ae8e356f5e9a1419e91a8
oOperaho <emanuelllevi7770@gmail.com>
2022-01-31 16:19:46 -0300

    →

--- Caesar/caesar.py
+++ Caesar/caesar.py
@@ -1,8 +1,12 @@
 def caesarciphr(w, k):
-    alphabet, out, l = " abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "", []
+    try:
+        alphabet, out, l = " abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "", []
 
-    for letter in w.lower():
-        out = alphabet.index(letter) + k
-        l.append(alphabet[out])
+        for letter in w.lower():
+            out = alphabet.index(letter) + k
+            l.append(alphabet[out])
 
-    return "".join(l)
+        return "".join(l)
+    except ValueError:
+        return "Error! Type a number!"
+
