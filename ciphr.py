11d55c634caf4f5c859b175561a694997cd21ca7
oOperaho <emanuelllevi7770@gmail.com>
2022-02-01 10:26:03 -0300

    →

--- ciphr.py
+++ ciphr.py
@@ -143,16 +143,22 @@ class CaesarTab(QWidget):
         self.result.setGeometry(640, 410, 150, 20)
         self.result.setStyleSheet("""background-color: #042c18; color: #dbf45c; border: 2px solid black;""")
         self.result.setFont(QFont("Helvetica", 10))
+        pos_x = self.width() - self.result.width()
+        self.result.move(int(pos_x / 2), 200)
 
         self.caesarkey.setText("3")
         self.caesarkey.setGeometry(685, 320, 50, 30)
         self.caesarkey.setFont(QFont("Arial", 8))
         self.caesarkey.setStyleSheet("""background-color: #042c18; color: #dbf45c; border: 2px solid black;""")
+        pos_x = self.width() - self.caesarkey.width()
+        self.caesarkey.move(int(pos_x / 2), 120)
 
         self.caesarinput.setText("word")
         self.caesarinput.setGeometry(655, 280, 120, 30)
         self.caesarinput.setFont(QFont("Arial", 10))
         self.caesarinput.setStyleSheet("""background-color: #042c18; color: #dbf45c; border: 2px solid black;""")
+        pos_x = self.width() - self.caesarinput.width()
+        self.caesarinput.move(int(pos_x / 2), 80)
 
         self.encodebutton.setText("Encode")
         self.encodebutton.setGeometry(620, 360, 90, 40)
