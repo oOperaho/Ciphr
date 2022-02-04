e71965e19f439172c8388f28437895e5c550e65e
oOperaho <emanuelllevi7770@gmail.com>
2022-02-03 23:13:14 -0300

    →

--- Vigenere/encoder.py
+++ Vigenere/encoder.py
@@ -1,46 +1,11 @@
 def vigenereencoder(w, k):
-    from operator import add
-
-    res, word, letters = "", "", " abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
-
-    if len(w) > len(k):
-        nkey = len(w) - len(k)
-        if nkey < len(k):
-            word = k[:nkey]
-            res = k + w
-        elif nkey > len(k):
-            if len(k) == 1:
-                res = k * (nkey + 1)
-            elif len(k) == 2:
-                res = k * nkey
-            else:
-                word = k + k[:nkey]
-                res = k + w
-        else:
-            res += k + k
-
+    k = list(k)
+    if len(w) == len(k):
+        return "".join(k)
     elif len(w) < len(k):
-        nkey = len(k) - len(w)
-        word = k[:-nkey]
-        res += w
-
+        return "Word has to be larger than key!"
     else:
-        res += k
+        for x in range(len(w) - len(k)):
+            k.append(k[x % len(k)])
+        return "".join(k)
 
-    s, l, fin, enc = [], [], [], []
-
-    for i in word:
-        s.append(letters.find(i))
-
-    for n in range(0, len(s)):
-        s[n] = s[n] - 1
-
-    for j in res:
-        l.append(letters.find(j))
-
-    fin = list(map(add, s, l))
-
-    for x in fin:
-        enc.append(letters[x])
-
-    return "".join(enc)
