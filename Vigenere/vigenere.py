553e9899031e3b24a034eca1e2f85282cb9093ea
oOperaho <emanuelllevi7770@gmail.com>
2022-02-04 18:53:56 -0300

    →

--- Vigenere/vigenere.py
+++ Vigenere/vigenere.py
@@ -1,8 +1,18 @@
-def vigenereencoder(w, k):
-
+def ciphertext(w, k):
+    l, c = list(k), 0
+    dif = abs(len(w) - len(k))
     while True:
+        if len(w) == len(k):
+            return "".join(l)
+        elif len(w) < len(k):
+            "".join(l)
+            l = l[:dif]
+            return "".join(l)
+        else:
+            l.append(k[c])
+            if len(l) == len(w):
+                break
+            c += 1
 
 
-
-def vigeneredecoder(cipher, key):
-
+print(ciphertext("banana", "oi"))
