f1c5061b515869c012cb3325364056daba227aa4
oOperaho <emanuelllevi7770@gmail.com>
2022-02-03 23:41:31 -0300

    →

--- Vigenere/encoder.py
+++ Vigenere/encoder.py
@@ -1,11 +1,30 @@
 def vigenereencoder(w, k):
-    k = list(k)
-    if len(w) == len(k):
-        return "".join(k)
-    elif len(w) < len(k):
-        return "Word has to be larger than key!"
-    else:
-        for x in range(len(w) - len(k)):
-            k.append(k[x % len(k)])
-        return "".join(k)
+    kl = len(k)
+    key_index, word_index = [], []
+    encrypt = ""
+    for x in k:
+        key_index = ord(x)
+    for y in w:
+        word_index = ord(y)
 
+    for i in range(len(word_index)):
+        c = (word_index[i] + key_index[i % kl]) % 26
+        encrypt += chr(c + 65)
+
+    return encrypt
+
+
+def vigeneredecoder(w, k):
+    kl = len(k)
+    key_index, word_index = [], []
+    decrypt = ""
+    for x in k:
+        key_index = ord(x)
+    for y in w:
+        word_index = ord(y)
+
+    for i in range(len(word_index)):
+        c = (word_index[i] + key_index[i % kl]) % 26
+        decrypt += chr(c + 65)
+
+    return decrypt
