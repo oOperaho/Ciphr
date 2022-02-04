6d501c1af16ff30ff30a19a83353e5134d5293f4
oOperaho <emanuelllevi7770@gmail.com>
2022-02-04 18:25:10 -0300

    →

--- Vigenere/vigenere.py
+++ Vigenere/vigenere.py
@@ -1,36 +1,8 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz "
+def vigenereencoder(w, k):
 
-letter_to_index = dict(zip(alphabet, range(len(alphabet))))
-index_to_letter = dict(zip(range(len(alphabet)), alphabet))
+    while True:
 
-
-def vigenereencoder(message, key):
-    encrypted = ""
-    split_message = [
-        message[i: i + len(key)] for i in range(0, len(message), len(key))
-    ]
-
-    for each_split in split_message:
-        i = 0
-        for letter in each_split:
-            number = (letter_to_index[letter] + letter_to_index[key[i]]) % len(alphabet)
-            encrypted += index_to_letter[number]
-            i += 1
-
-    return encrypted
 
 
 def vigeneredecoder(cipher, key):
-    decrypted = ""
-    split_encrypted = [
-        cipher[i: i + len(key)] for i in range(0, len(cipher), len(key))
-    ]
 
-    for each_split in split_encrypted:
-        i = 0
-        for letter in each_split:
-            number = (letter_to_index[letter] - letter_to_index[key[i]]) % len(alphabet)
-            decrypted += index_to_letter[number]
-            i += 1
-
-    return decrypted
